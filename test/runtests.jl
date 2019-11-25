038a4ea7b36b239161a1d2068e6b208e793ed7c6
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-11-25 15:07:50 -0500

    Make tests compatible with julia <v"1.2"
    This is the better spelling, anyway.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ onfail(body, _::Tuple{Test.Fail,T}) where {T} = body()
 
 #TODO test transforms!
 @testset "one-point hulls" begin
-    for GJK_REAL = [Cdouble, Cfloat], GJK_DIM = 3:3, tr = [nothing, Matrix{GJK_REAL}(I(GJK_DIM+1)[1:GJK_DIM+1, 1:GJK_DIM])], i = 1:100
+    for GJK_REAL = [Cdouble, Cfloat], GJK_DIM = 3:3, tr = [nothing, Matrix{GJK_REAL}(I, GJK_DIM+1, GJK_DIM)], i = 1:100
         p1 = tuple(rand(GJK_REAL, GJK_DIM)...)
         p2 = tuple(rand(GJK_REAL, GJK_DIM)...)
         r = GJK_Reference.gjk_distance([p1], tr, [p2], tr)
